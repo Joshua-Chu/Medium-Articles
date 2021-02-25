4929d6333dc8cac092092c9cb307e590157d0a63
unknown <jpchu@up.edu.ph>
2021-02-25 10:48:48 +0800

    naming_variables

---
+++ naming_variables/naming.js
@@ -0,0 +1,20 @@
+//instead of naming your variable like this
+
+const var1 = "joshua";
+
+// you could be more specific and help your self out a little bit
+
+const userName = "joshua";
+
+
+// I often name my functions this way
+
+function func1 (){
+    console.log(`${userName} has logged in`);
+}
+
+// You could do this to save future confusion
+
+function logIn(){
+    console.log(`${userName} has logged in`);
+}
